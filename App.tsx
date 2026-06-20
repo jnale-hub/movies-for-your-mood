ebb70d04f21343ee7cd6839122c964f10c1a8e6e
John Mark Delima <delimajohnmark14@gmail.com>
2026-06-20 12:58:18 +0800

    Initialize new app

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
-
+import { VibeCheck } from './src/screens/VibeCheck';
 import './global.css';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
+    <VibeCheck />
   );
 }
